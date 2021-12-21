214f0618857a9a904ff3dc38aeb4c11af73b0c5c
david2203 <david.boes22@gmail.com>
2021-12-21 11:50:35 +0100

    Revert "moien"
    This reverts commit 5fab7115d8012fba4735813ec3411651cf58e78c.

--- Gamefolder_HowdyCowboy/Player.gd
+++ Gamefolder_HowdyCowboy/Player.gd
@@ -32,25 +32,3 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-
-
-func _on_Player_ready(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	pass # Replace with function body.
