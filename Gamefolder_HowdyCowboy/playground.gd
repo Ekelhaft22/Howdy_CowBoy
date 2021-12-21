5fab7115d8012fba4735813ec3411651cf58e78c
david2203 <david.boes22@gmail.com>
2021-12-21 11:46:08 +0100

    moien

--- Gamefolder_HowdyCowboy/playground.gd
+++ Gamefolder_HowdyCowboy/playground.gd
@@ -1,18 +1,31 @@
-extends Node2D
-const MyClass = preload("Player.gd")
+extends Node
+#const Player = preload("Player.gd")
+export (PackedScene) var Player
 #https://docs.godotengine.org/de/stable/getting_started/scripting/gdscript/gdscript_basics.html#classes-as-resources
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+#var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Hier müssen die Player noch instanziiert werden!
-	var player = MyClass.new()
-	pass # Replace with function body.
+	#player = Player.new()
+	#player._init()
+	#$Player.start()
+	#player._ready()
+	#get_node("Player").connect("ready", self,"_ready")
+	#pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#Player._on_Player_ready(delta)
+	#Player = Player.instance()
+	#add_child(Player)
+	
+	#Player._on_Player_ready(delta)
+	#player._process(delta)
+	#get_node("player").connect("process", self,"_process")
+	pass
